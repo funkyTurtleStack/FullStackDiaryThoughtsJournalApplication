e7acf966587c75eb3472c08a783cb24642ff6e16
funkyTurtleStack <braedon.stapelman@drake.edu>
2025-12-15 18:52:43 -0600

    completed tags.routes.js

--- backend/routes/tags.routes.js
+++ backend/routes/tags.routes.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User.js");
 const { requireAuth } = require("../middleware/auth.middleware.js");
 const Tag = require("../models/Tag");
 
@@ -15,7 +13,7 @@ router.post("/", requireAuth, async (req, res) => {
     try{
         const tag = new Tag({
             name: req.body.name,
-            user: req.userId
+            user: req.userId           
         });
 
         const savedTag = await tag.save();
@@ -29,25 +27,69 @@ router.post("/", requireAuth, async (req, res) => {
 /**
  * GET /api/tags
  * Description: get the list of tags
+ * Access: Private
  */
-router.get("/", async (req, res) => {
+router.get("/", requireAuth, async (req, res) => {
     try{
+        const tags = await Tag.find({ user: req.userId });
         
+        res.json(tags);
     }
     catch (err){
-
+        res.status(500).json({message: err.message});
     }
 });
 
 /**
  * PUT /api/tags:id
  * Description: change a tag
+ * Access: Private
  */
+router.put("/:id", requireAuth, async (req, res) => {
+    try{
+        const changedTag = await Tag.findOneAndUpdate(
+            {_id: req.params.id, user: req.userId},
+            {name: req.body.name},
+            {new: true, runValidators: true}
+        );
+
+        if(!changedTag){
+            return res.status(404).json({message: "Tag not found"});
+        }
+
+        res.json(changedTag);
+    }
+    catch (err){
+        if(err.name === "ValidationError"){
+            return res.status(400).json({message: err.message});
+        }
+
+        res.status(500).json({message: err.message});
+    }
+});
 
 /**
  * DELETE /api/tags/:id
  * Description: delete a tag
+ * Access: Private
  */
+router.delete("/:id", requireAuth, async (req, res) => {
+    try{
+        const deletedTag = await Tag.findOneAndDelete({
+        _id: req.params.id,
+        user: req.userId
+        })
+
+        if(!deletedTag){
+            return res.status(404).json("Tag not found");
+        }
+
+        res.json({"message": "Tag deleted successfully", "tag": deletedTag});
+    }
+    catch (err){
+        res.status(500).json({message: err.message});
+    }
+});
 
 
 module.exports = router;
