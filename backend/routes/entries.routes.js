64cd5a395f7eb459568714cf0bf0888919f22065
happy-turtle-stack <braedon.stapelman@drake.edu>
2025-12-15 18:59:23 -0600

    updated entries.routes.js again

--- backend/routes/entries.routes.js
+++ backend/routes/entries.routes.js
@@ -5,5 +5,32 @@ const Tag = require("../models/Entry");
 const router = express.Router();
 
 
+//(allow for filtering by tags AND sorting by tags AND sorting by (date OR alphabetical))
+/**
+ * GET /api/entries
+ * Description: get the list of entries
+ * Access: Private
+ */
+
+
+/**
+ * POST /api/entries - make a new entry
+ */
+
+
+/**
+ * GET /api/entries/:id - get an individual entry
+ */
+
+
+/**
+ * PUT /api/entries/:id - update an entry
+ */
+
+
+/**
+ * DELETE /api/entries/:id - delete an entry
+ */
+
 
 module.exports = router;
