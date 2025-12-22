8c455309def26ae2830721ac2654a924c94cd1fd
happy-turtle-stack <braedon.stapelman@drake.edu>
2025-12-22 12:31:07 -0600

    sameSite settings for cookies changed to "none"

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -24,7 +24,7 @@ router.post("/login", async (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
